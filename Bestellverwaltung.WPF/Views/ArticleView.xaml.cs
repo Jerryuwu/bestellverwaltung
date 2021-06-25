09eaecad71fa0fe0cf8e1543224247a97b2afd92
Jerry <jjjdormann@gmail.com>
2021-06-25 18:41:26 +0200

    first commit

---
+++ Bestellverwaltung.WPF/Views/ArticleView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using ReactiveUI;
+
+namespace Bestellverwaltung.WPF.Views {
+    public partial class ArticleView {
+        public ArticleView() {
+            InitializeComponent();
+            ViewModel = new();
+            this.WhenActivated(disposable => {
+
+            });
+        }
+    }
+}
