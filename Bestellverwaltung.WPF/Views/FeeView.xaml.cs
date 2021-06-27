5480d6df78aeaf6eed7b4df6b9e2cd5adc7112b3
Jerry <jjjdormann@gmail.com>
2021-06-27 16:06:11 +0200

    push

---
+++ Bestellverwaltung.WPF/Views/FeeView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using ReactiveUI;
+
+namespace Bestellverwaltung.WPF.Views {
+  public partial class FeeView {
+    public FeeView() {
+      InitializeComponent();
+      ViewModel = new();
+      this.WhenActivated(disposable => {
+
+      });
+    }
+  }
+}
