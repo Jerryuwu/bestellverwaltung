77bea4bd3cfba40369a20fbd4fd08b34ec1981c9
Jerry <jjjdormann@gmail.com>
2021-07-05 11:22:53 +0200

    push

---
+++ Bestellverwaltung.WPF/ViewModels/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Mail;
+using ReactiveUI;
+using Splat;
+
+namespace Bestellverwaltung.WPF.ViewModels {
+    public class OrderViewModel : ReactiveObject, IRoutableViewModel, IActivatableViewModel {
+        public string? UrlPathSegment => "Orders";
+        public IScreen HostScreen { get; }
+        public ViewModelActivator Activator { get; }
+
+        public OrderViewModel() {
+            Activator = new();
+            HostScreen = Locator.Current.GetService<IScreen>();
+        }
+    }
+}
---
+++ Bestellverwaltung.WPF/Views/OrderView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+using DynamicData.Aggregation;
+using DynamicData.Diagnostics;
+
+namespace Bestellverwaltung.WPF.Views {
+    public partial class OrderView {
+        public OrderView() {
+            InitializeComponent();
+        }
+    }
+}
